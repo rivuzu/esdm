23ef5284f8bc74a7de2a39f75b93c498f3352a52
Morsv <rpj_morsvmail@yahoo.com>
2020-02-24 20:36:41 +0700

    cadetreadiness timer

--- lib/src/CadetReadiness/index.dart
+++ lib/src/CadetReadiness/index.dart
@@ -10,19 +10,38 @@ enum FormType { login, register }
 
 class _CadetReadinessState extends State<CadetReadiness>{
 
-  bool visibilityTag = false;
-  bool visibilityObs = false;
+  bool visibilitySitUp = false;
+  bool visibilityPushUp = false;
 
   void _changed(bool visibility, String field) {
     setState(() {
-      if (field == "tag"){
-        visibilityTag = visibility;
+      if (field == "SitUp"){
+        visibilitySitUp = visibility;
       }
-      if (field == "obs"){
-        visibilityObs = visibility;
+      if (field == "PushUp"){
+        visibilityPushUp = visibility;
       }
     });
   }
+
+  Timer _timer;
+int _start = 0;
+
+void startTimer() {
+  const oneSec = const Duration(seconds: 1);
+  _timer = new Timer.periodic(
+    oneSec,
+    (Timer timer) => setState(
+      () {
+        if (_start < 0) {
+          timer.cancel();
+        } else {
+          _start = _start + 1;
+        }
+      },
+    ),
+  );
+}
 
   @override
   Widget build(BuildContext context){
@@ -38,35 +57,36 @@ class _CadetReadinessState extends State<CadetReadiness>{
             margin: new EdgeInsets.only(left: 16.0, right: 16.0),
             child: new Column(
               children: <Widget>[
-                visibilityObs ? new Row(
+                visibilityPushUp ? new Row(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
+                    
                     new Expanded(
                       flex: 11,
-                      child: new TextField(
-                        maxLines: 1,
-                        style: Theme.of(context).textTheme.title,
-                        decoration: new InputDecoration(
-                          labelText: "Observation",
-                          isDense: true
-                        ),
-                      ),
+                      child:   RaisedButton(
+                        onPressed: () {
+                          startTimer();
+                        },
+                        child: Text("start"),
+                      ), 
                     ),
+                    new SizedBox(height: 24.0),
+                    Text("$_start"),
                     new Expanded(
                       flex: 1,
                       child: new IconButton(
                         color: Colors.grey[400],
                         icon: const Icon(Icons.cancel, size: 22.0,),
                         onPressed: () {
-                          _changed(false, "obs");
+                          _changed(false, "PushUp");
                         },
                       ),
                     ),
                   ],
                 ) : new Container(),
 
-                visibilityTag ? new Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                visibilitySitUp ? new Row(
+                  // crossAxisAlignment: CrossAxisAlignment.end,
                   children: <Widget>[
                     new Expanded(
                       flex: 11,
@@ -74,7 +94,7 @@ class _CadetReadinessState extends State<CadetReadiness>{
                         maxLines: 1,
                         style: Theme.of(context).textTheme.title,
                         decoration: new InputDecoration(
-                          labelText: "Tags",
+                          labelText: "SitUp",
                           isDense: true
                         ),
                       ),
@@ -85,7 +105,7 @@ class _CadetReadinessState extends State<CadetReadiness>{
                         color: Colors.grey[400],
                         icon: const Icon(Icons.cancel, size: 22.0,),
                         onPressed: () {
-                          _changed(false, "tag");
+                          _changed(false, "SitUp");
                         },
                       ),
                     ),
@@ -101,13 +121,13 @@ class _CadetReadinessState extends State<CadetReadiness>{
             children: <Widget>[
               new InkWell(
                 onTap: () {
-                  visibilityObs ? null : _changed(true, "obs");
+                  visibilityPushUp ? null : _changed(true, "PushUp");
                 },
                 child: new Container(
                   margin: new EdgeInsets.only(top: 16.0),
                   child: new Column(
                     children: <Widget>[
-                      new Icon(Icons.comment, color: visibilityObs ? Colors.grey[400] : Colors.grey[600]),
+                      new Icon(Icons.comment, color: visibilityPushUp ? Colors.grey[400] : Colors.grey[600]),
                       new Container(
                         margin: const EdgeInsets.only(top: 8.0),
                         child: new Text(
@@ -115,7 +135,7 @@ class _CadetReadinessState extends State<CadetReadiness>{
                           style: new TextStyle(
                             fontSize: 12.0,
                             fontWeight: FontWeight.w400,
-                            color: visibilityObs ? Colors.grey[400] : Colors.grey[600],
+                            color: visibilityPushUp ? Colors.grey[400] : Colors.grey[600],
                           ),
                         ),
                       ),
@@ -126,13 +146,13 @@ class _CadetReadinessState extends State<CadetReadiness>{
               new SizedBox(width: 24.0),
               new InkWell(
                 onTap: () {
-                  visibilityTag ? null : _changed(true, "tag");
+                  visibilitySitUp ? null : _changed(true, "tag");
                 },
                 child: new Container(
                   margin: new EdgeInsets.only(top: 16.0),
                   child: new Column(
                     children: <Widget>[
-                      new Icon(Icons.local_offer, color: visibilityTag ? Colors.grey[400] : Colors.grey[600]),
+                      new Icon(Icons.local_offer, color: visibilitySitUp ? Colors.grey[400] : Colors.grey[600]),
                       new Container(
                         margin: const EdgeInsets.only(top: 8.0),
                         child: new Text(
@@ -140,7 +160,7 @@ class _CadetReadinessState extends State<CadetReadiness>{
                           style: new TextStyle(
                             fontSize: 12.0,
                             fontWeight: FontWeight.w400,
-                            color: visibilityTag ? Colors.grey[400] : Colors.grey[600],
+                            color: visibilitySitUp ? Colors.grey[400] : Colors.grey[600],
                           ),
                         ),
                       ),
@@ -151,13 +171,13 @@ class _CadetReadinessState extends State<CadetReadiness>{
               new SizedBox(width: 24.0),
               new InkWell(
                 onTap: () {
-                  visibilityTag ? null : _changed(true, "tag");
+                  visibilitySitUp ? null : _changed(true, "tag");
                 },
                 child: new Container(
                   margin: new EdgeInsets.only(top: 16.0),
                   child: new Column(
                     children: <Widget>[
-                      new Icon(Icons.local_offer, color: visibilityTag ? Colors.grey[400] : Colors.grey[600]),
+                      new Icon(Icons.local_offer, color: visibilitySitUp ? Colors.grey[400] : Colors.grey[600]),
                       new Container(
                         margin: const EdgeInsets.only(top: 8.0),
                         child: new Text(
@@ -165,7 +185,7 @@ class _CadetReadinessState extends State<CadetReadiness>{
                           style: new TextStyle(
                             fontSize: 12.0,
                             fontWeight: FontWeight.w400,
-                            color: visibilityTag ? Colors.grey[400] : Colors.grey[600],
+                            color: visibilitySitUp ? Colors.grey[400] : Colors.grey[600],
                           ),
                         ),
                       ),
