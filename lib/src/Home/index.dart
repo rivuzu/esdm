13ed274ddb257feba68cb950ad9dd4638b7bb4fb
deno <denotm12@gmail.com>
2020-02-25 02:16:31 +0700

    add EMC2

--- lib/src/Home/index.dart
+++ lib/src/Home/index.dart
@@ -1,4 +1,5 @@
 import 'package:esdm/src/CadetReadiness/index.dart';
+import 'package:esdm/src/EMC/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -24,7 +25,7 @@ List<Widget> _tiles = const <Widget>[
 
   const _GridMenu(Colors.green, Icons.widgets, Text('Catpers', style: TextStyle(color: Colors.white),)),
   const _CadetReadiness(Colors.amber, Icons.directions_run, Text('Cadet Readiness', style: TextStyle(color: Colors.white),)),
-  const _GridMenu(Colors.black, Icons.chat, Text('EMC2', style: TextStyle(color: Colors.white),)),
+  const _EMC(Colors.black, Icons.chat, Text('EMC2', style: TextStyle(color: Colors.white),)),
   const _GridMenu(Colors.deepOrange, Icons.markunread, Text('Disposisi', style: TextStyle(color: Colors.white),)),
   const _GridMenu(Colors.brown, Icons.keyboard_arrow_up, Text('Kalkulator Pangkat', style: TextStyle(color: Colors.white),)),
   const _GridMenu(Colors.red, Icons.note, Text('Tryout Psi', style: TextStyle(color: Colors.white),)),
@@ -154,6 +155,53 @@ class _CadetReadiness extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => CadetReadiness()),
+              );
+            },
+        child: new Center(
+          child: new Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    //text
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Icon(iconData, color: Colors.white, size: 40.0,),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: textGrid
+                      )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _EMC extends StatelessWidget {
+  const _EMC(this.backgroundColor, this.iconData, this.textGrid);
+
+  final Color backgroundColor;
+  final IconData iconData;
+  final Text textGrid;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      color: backgroundColor,
+      child: new InkWell(
+        onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => EMC()),
               );
             },
         child: new Center(
