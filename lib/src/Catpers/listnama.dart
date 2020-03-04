77025f76b04b634e9b2909850ced7bd35813c851
Aghi <rizkyputrajunior@gmail.com>
2020-03-04 16:56:08 +0700

    still working progress load listview with local storage

--- lib/src/Catpers/listnama.dart
+++ lib/src/Catpers/listnama.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ListNama extends StatefulWidget {
   @override
@@ -8,6 +9,27 @@ class ListNama extends StatefulWidget {
 class _ListNamaState extends State<ListNama> {
   var items = ["Adi Mansyur", "Adi Muhammad", "Adi Setiawan", "Adi Teguh", "Aditya Radit", "Adib Pratama", "Adit Reza"];
   var colors = [Colors.red, Colors.red, Colors.green, Colors.red, Colors.green, Colors.green, Colors.red];
+  // List<String> litems = myStringList;
+  
+  _writeData() async{
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('my_string_list_key', ["Adi Mansyur", "Adi Muhammad", "Adi Setiawan", "Adi Teguh", "Aditya Radit", "Adib Pratama", "Adit Reza", "Adit Geiza", "Adib Rasyid"]);
+  }
+
+  _getData() async{
+    final prefs = await SharedPreferences.getInstance();
+    final myStringList = prefs.getStringList('my_string_list_key') ?? [];
+    print(myStringList);
+    return myStringList;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _writeData();
+    _getData();
+  }
 
   @override
   Widget build(BuildContext context) {
