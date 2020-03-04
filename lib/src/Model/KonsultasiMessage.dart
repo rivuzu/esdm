135e75b177699fc9961f9c813a7bd97ca5df2d41
ipulsamudin <ipulsamudin@gmail.com>
2020-03-04 09:27:21 +0700

    menampilkan room list masih duplicat

--- lib/src/Model/KonsultasiMessage.dart
+++ lib/src/Model/KonsultasiMessage.dart
@@ -1,17 +1,26 @@
 class KonsultasiMessage{
   String message;
   String sender;
+  String id_sender;
+  String id_chat;
+  String role;
 
-  KonsultasiMessage({this.message, this.sender});
+  KonsultasiMessage({this.message, this.sender,this.id_sender,this.id_chat,this.role});
   KonsultasiMessage.fromJson(Map<String, dynamic> json) {
     message = json['message'];
     sender = json['sender'];
+    id_sender = json['id_sender'];
+    id_chat = json['id_chat'];
+    role = json['role'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['message'] = this.message;
     data['sender'] = this.sender;
+    data['id_sender'] = this.id_sender;
+    data['id_chat'] = this.id_chat;
+    data['role'] = this.role;
     return data;
   }
 }
