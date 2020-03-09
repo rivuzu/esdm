d346c34e89048b6f879770e5f84bd0f20f9435c4
Aghi <rizkyputrajunior@gmail.com>
2020-03-09 14:41:18 +0700

    lari 12 menit

--- lib/src/CadetReadiness/lari12.dart
+++ lib/src/CadetReadiness/lari12.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -7,12 +8,19 @@ class Lari12 extends StatefulWidget {
   _Lari12State createState() => _Lari12State();
 }
 
+int _start = 0;
 int _minute = 0;
 int _seconds = 00;
-String _zero = "0";
+String _zero = "";
+bool _filledNumber = false;
+final _numberInput = TextEditingController();
+String _tickerTime;
 Timer _timer;
 bool _timeRunning = false;
 bool _resetTime = false;
+bool buttonMulai = true;
+bool buttonJeda = false;
+bool rowJeda = false;
 
 class _Lari12State extends State<Lari12> {
   CameraPosition _kGooglePlex = CameraPosition(
@@ -26,15 +34,23 @@ class _Lari12State extends State<Lari12> {
       oneSec,
       (Timer timer) => setState(
         () {
-          if (_seconds == 59) {
-            _minute = _minute + 1;
-            _zero = "0";
-            _seconds = 0;
+          if (_minute < 1 && _seconds < 1) {
+            _filledNumber = false;
+            timer.cancel();
           } else {
-            if (_seconds >= 9) {
+            if (_seconds == 00) {
+              _minute = _minute - 1;
               _zero = "";
+              _seconds = 59;
+            } else {
+              if (_seconds <= 10) {
+                print(_seconds);
+                _zero = "0";
+              } else if (_seconds > 10) {
+                _zero = "";
+              }
+              _seconds = _seconds - 1;
             }
-            _seconds = _seconds + 1;
           }
         },
       ),
@@ -42,55 +58,299 @@ class _Lari12State extends State<Lari12> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    _timer.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _start = 720;
+      _minute = (_start / 60).floor();
+      // print(_minute);
+      _seconds = _start - _minute * 60;
+      _tickerTime = "$_minute:$_zero$_seconds";
+      if (_seconds < 10) {
+        // print(_seconds);
+        _zero = "0";
+      } else if (_seconds > 10) {
+        _zero = "";
+      }
+      if (_start != 0) {
+        _filledNumber = true;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     Completer<GoogleMapController> _controller = Completer();
-    return new Stack(
+    // return new Stack(
+    //   children: <Widget>[
+    //     new Column(
+    //       children: <Widget>[
+    //         new Container(
+    //             child: new Text(
+    //           "$_minute:$_zero$_seconds",
+    //           style: new TextStyle(
+    //               fontWeight: FontWeight.bold,
+    //               fontSize: 70.0,
+    //               fontFamily: 'Roboto'),
+    //         )),
+    //         new Row(
+    //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //           children: <Widget>[
+    //             _timeRunning ? stopButton() : startButton(),
+    //             _resetTime
+    //                 ? new RaisedButton(
+    //                     onPressed: () {
+    //                       setState(() {
+    //                         _minute = 0;
+    //                         _seconds = 0;
+    //                         _zero = "0";
+    //                         _resetTime = !_resetTime;
+    //                       });
+    //                     },
+    //                     textColor: Colors.white,
+    //                     color: Colors.blue,
+    //                     padding: const EdgeInsets.all(8.0),
+    //                     child: new Text(
+    //                       "Reset",
+    //                     ),
+    //                   )
+    //                 : Container()
+    //           ],
+    //         )
+    //       ],
+    //     ),
+    //     new Container(
+    //         margin: EdgeInsets.only(top: 150),
+    //         child: new GoogleMap(
+    //           mapType: MapType.normal,
+    //           initialCameraPosition: _kGooglePlex,
+    //           onMapCreated: (GoogleMapController controller) {
+    //             _controller.complete(controller);
+    //           },
+    //         ))
+    //   ],
+    // );
+    return Stack(
       children: <Widget>[
-        new Column(
-          children: <Widget>[
-            new Container(
-                child: new Text(
-              "$_minute:$_zero$_seconds",
-              style: new TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 70.0,
-                  fontFamily: 'Roboto'),
-            )),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _timeRunning ? stopButton() : startButton(),
-                _resetTime
-                    ? new RaisedButton(
-                        onPressed: () {
-                          setState(() {
-                            _minute = 0;
-                            _seconds = 0;
-                            _zero = "0";
-                            _resetTime = !_resetTime;
-                          });
-                        },
-                        textColor: Colors.white,
-                        color: Colors.blue,
-                        padding: const EdgeInsets.all(8.0),
-                        child: new Text(
-                          "Reset",
+        new Container(
+          child: new Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 30.0,
+              ),
+              Icon(
+                Icons.person,
+                size: 150,
+                color: Colors.blue,
+              ),
+              DottedBorder(
+                borderType: BorderType.RRect,
+                radius: Radius.circular(12),
+                padding: EdgeInsets.all(8.0),
+                strokeWidth: 2,
+                dashPattern: [5, 8],
+                color: Colors.blue,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.transparent,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          "$_minute",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 60.0,
+                            fontFamily: 'Roboto',
+                          ),
                         ),
-                      )
-                    : Container()
-              ],
-            )
-          ],
+                        Text(
+                          "Min",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45.0,
+                            fontFamily: 'Roboto',
+                          ),
+                        ),
+                        Text(
+                          "$_zero$_seconds",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 60.0,
+                            fontFamily: 'Roboto',
+                          ),
+                        ),
+                        Text(
+                          "Sec",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 45.0,
+                            fontFamily: 'Roboto',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Visibility(
+                visible: buttonMulai,
+                child: RaisedButton(
+                  onPressed: () {
+                    startTimer();
+                    _timeRunning = true;
+                    setState(() {
+                      buttonMulai = false;
+                    });
+                    setState(() {
+                      buttonJeda = true;
+                    });
+                    setState(() {
+                      rowJeda = false;
+                    });
+                  },
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                    // side: BorderSide(color: Colors.red)
+                  ),
+                  child: Text(
+                    "Mulai",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: buttonJeda,
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonMulai = false;
+                    });
+                    setState(() {
+                      buttonJeda = false;
+                    });
+                    setState(() {
+                      rowJeda = true;
+                    });
+                    _timer.cancel();
+                    _timeRunning = false;
+                  },
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                    // side: BorderSide(color: Colors.red)
+                  ),
+                  child: Text(
+                    "Jeda",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Visibility(
+                visible: rowJeda,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          buttonMulai = false;
+                        });
+                        setState(() {
+                          buttonJeda = true;
+                        });
+                        setState(() {
+                          rowJeda = false;
+                        });
+                        startTimer();
+                      },
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10.0),
+                        // side: BorderSide(color: Colors.red)
+                      ),
+                      child: Text(
+                        "Lanjutkan",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          buttonMulai = true;
+                        });
+                        setState(() {
+                          buttonJeda = false;
+                        });
+                        setState(() {
+                          rowJeda = false;
+                        });
+                        setState(() {
+                          _zero = "0";
+                          _minute = 12;
+                          _seconds = 00;
+                          _timeRunning = false;
+                        });
+                      },
+                      color: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10.0),
+                        // side: BorderSide(color: Colors.red)
+                      ),
+                      child: Text(
+                        "Reset",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         new Container(
-            margin: EdgeInsets.only(top: 150),
-            child: new GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: _kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-            ))
+          margin: EdgeInsets.only(top: 350),
+          height: MediaQuery.of(context).size.height / 4,
+          width: MediaQuery.of(context).size.width,
+          child: new GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ),
       ],
     );
   }
